d1d4b2ac1d04d56775df76367079dc7564492d97
huangxjia <2819335912@qq.com>
2014-05-26 05:14:45 +0800

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -4,14 +4,12 @@ trainFold <- dir('UCI HAR Dataset/train')[-1]
 testFold <- dir('UCI HAR Dataset/test')[-1]
 
 ##read data into variables
-
 train <- lapply(as.list(trainFold), function(x) read.table(paste0(getwd(), '/UCI HAR Dataset/train/', x)))
 test <- lapply(as.list(testFold), function(x) read.table(paste0(getwd(), '/UCI HAR Dataset/test/', x)))
 features <- read.table("./UCI HAR Dataset/features.txt")
 activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 ##set column names with names in the 'features.txt'
-
 colnames(train[[1]]) <- "volunteersID"
 colnames(test[[1]]) <- "volunteersID"
 colnames(train[[2]]) <- features[ , 2]
@@ -19,15 +17,13 @@ colnames(test[[2]]) <- features[ , 2]
 colnames(train[[3]]) <- "Activities"
 colnames(test[[3]]) <- "Activities"
 
-##Firstly merge the training and test activities labels, subjects information and ##measurements with cbind and then merge train data and test data with rbind.
-
+##Firstly merge the training and test activities labels, subjects ##information and ##measurements with cbind and then merge train data ##and test data with rbind.
 mergeTrain <- cbind(train[[1]], train[[3]], train[[2]])
 mergeTest <- cbind(test[[1]], test[[3]], test[[2]])
 totalMerge <- rbind(mergeTrain, mergeTest)
 
 
 ##step 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-
 ##subset columns with string "mean" or "std"
 
 meanStdIndex <- grep("mean|std", features$V2)
@@ -41,8 +37,8 @@ mergeTest2 <- cbind(test[[1]], test[[3]], subtest2)
 totalMerge2 <- rbind(mergeTrain2, mergeTest2)
 
 ##Step 3. Uses descriptive activity names to name the activities in the data set
-##Use the activity_labels.txt file to change these numbers into their corresponding ##names
 
+##Use the activity_labels.txt file to change these numbers into their corresponding ##names
 for (i in seq_len(nrow(train[[3]]))) {
   temp <- train[[3]][i, ]
   train[[3]][i, ] <- as.character(activityLabels[activityLabels$V1 
@@ -58,7 +54,6 @@ for (i in seq_len(nrow(test[[3]]))) {
 
 ##step 5. Creates a second, independent tidy data set with the average of each variable 
 ##for each activity and each subject. 
-
 meanIndex <- grep("mean", features$V2)
 meanIndex <- meanIndex[ !(meanIndex %in% meanFreqIndex)]
 subtrain5 <- train[[2]][ , meanIndex]
@@ -69,7 +64,6 @@ mergeTest5 <- cbind(test[[1]], test[[3]], subtest5)
 totalMerge5 <- rbind(mergeTrain5, mergeTest5)
 
 ##step 4. Appropriately labels the data set with descriptive activity names. 
-
 ##replace the abbreviation expression with full length expression
 s1 <- colnames(totalMerge5)
 s2 <- gsub("-mean()-", "Mean", s1, fixed = TRUE)
@@ -84,7 +78,6 @@ s10 <- gsub("Mag", "Magnitude", s9, fixed = TRUE)
 s13 <- s10
 
 ##Reorganize the variable names to be more descriptive.
-
 for (i in c(3:17, 23:31))
 {
   str <- strsplit(s13[i], "Mean")
@@ -100,7 +93,6 @@ for (i in c(18:22, 32:35))
 
 ##step 5. Creates a second, independent tidy data set with the average of each variable 
 ##for each activity and each subject.
-
 aql <- melt(totalMerge5, id.vars = c("volunteersID", "Activities"))
 tidyData <- dcast(aql, volunteersID + Activities ~ variable, fun.aggregate = mean)
 colnames(tidyData) <- s13
